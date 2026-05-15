0840696edf28f33ea4bc7685d0183c21b73ba9a2
NepJr <diehardsteam@gmail.com>
2026-05-15 10:58:24 -0700

    ex nihilo module refactor

--- src/main/java/xyz/nepjr/tech/compat/exnihilo/ExNihiloGTModule.java
+++ src/main/java/xyz/nepjr/tech/compat/exnihilo/ExNihiloGTModule.java
@@ -13,6 +13,8 @@ import exnihilocreatio.util.ItemInfo;
 import gregtech.api.GTValues;
 import gregtech.api.unification.material.Material;
 import gregtech.api.unification.material.Materials;
+import net.minecraft.block.Block;
+import net.minecraft.init.Blocks;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 
@@ -20,105 +22,44 @@ public class ExNihiloGTModule implements IExNihiloCreatioModule, IRecipeDefaults
 {
 	public void registerOreChunks(OreRegistry registry) 
 	{	
+		// Shouldn't need to check if these four are added, but only here incase someone adds extra mods to the pack
 		if(!registry.isRegistered("copper"))
 		{
-			registerMaterial(registry, Materials.Copper);
+			registerMaterial(registry, Materials.Copper, false, false);
 		}
 		if(!registry.isRegistered("tin"))
 		{
-			registerMaterial(registry, Materials.Tin);
+			registerMaterial(registry, Materials.Tin, false, false);
 		}
 		if(!registry.isRegistered("silver"))
 		{
-			registerMaterial(registry, Materials.Silver);
-		}
-		if(!registry.isRegistered("lead"))
-		{
-			registerMaterial(registry, Materials.Lead);
-		}
-		if(!registry.isRegistered("cobaltite"))
-		{
-			registerMaterial(registry, Materials.Cobaltite);
-		}
-		if(!registry.isRegistered("sphalerite"))
-		{
-			registerMaterial(registry, Materials.Sphalerite);
-		}
-		if(!registry.isRegistered("calcite"))
-		{
-			registerMaterial(registry, Materials.Calcite);
-		}
-		if(!registry.isRegistered("gypsum"))
-		{
-			registerMaterial(registry, Materials.Gypsum);
+			registerMaterial(registry, Materials.Silver, false, false);
 		}
 		if(!registry.isRegistered("sulfur"))
 		{
-			registerMaterial(registry, Materials.Sulfur);
-		}
-		if(!registry.isRegistered("oilsands"))
-		{
-			registerMaterial(registry, Materials.Oilsands);
-		}
-		if(!registry.isRegistered("tungstate"))
-		{
-			registerMaterial(registry, Materials.Tungstate);
-		}
-		if(!registry.isRegistered("scheelite"))
-		{
-			registerMaterial(registry, Materials.Scheelite);
+			registerMaterial(registry, Materials.Sulfur, true, true);
 		}
 		
-		ItemOre oreCalcite = ExNihiloRegistryManager.ORE_REGISTRY.getOreItem("calcite");
-		ItemOre oreGypsum = ExNihiloRegistryManager.ORE_REGISTRY.getOreItem("gypsum");
-		ItemOre oreSulfur = ExNihiloRegistryManager.ORE_REGISTRY.getOreItem("sulfur");
-		ItemOre oreOilsands = ExNihiloRegistryManager.ORE_REGISTRY.getOreItem("oilsands");
-		ItemOre oreSphalerite = ExNihiloRegistryManager.ORE_REGISTRY.getOreItem("sphalerite");
-		ItemOre oreCobaltite = ExNihiloRegistryManager.ORE_REGISTRY.getOreItem("cobaltite");
-		
-        registry.getSieveBlackList().add(oreCalcite);
-        registry.getSieveBlackList().add(oreGypsum);
-        registry.getSieveBlackList().add(oreSulfur);
-        registry.getSieveBlackList().add(oreOilsands);
-        registry.getSieveBlackList().add(oreSphalerite);
-        registry.getSieveBlackList().add(oreCobaltite);   
+		registerMaterial(registry, Materials.Galena, true, true);
+		registerMaterial(registry, Materials.Cobaltite, true, true);
+		registerMaterial(registry, Materials.Sphalerite, true, true);
+		registerMaterial(registry, Materials.Calcite, true, true);
+		registerMaterial(registry, Materials.Gypsum, true, true);
+		registerMaterial(registry, Materials.Oilsands, true, true);
+		registerMaterial(registry, Materials.Tungstate, true, true);
+		registerMaterial(registry, Materials.Scheelite, true, true);
 	}
 	
 	public void registerSieve(SieveRegistry registry) {
-        ItemOre sulfur = ExNihiloRegistryManager.ORE_REGISTRY.getOreItem("sulfur");
-        ItemOre calcite = ExNihiloRegistryManager.ORE_REGISTRY.getOreItem("calcite");
-        ItemOre gypsum = ExNihiloRegistryManager.ORE_REGISTRY.getOreItem("gypsum");
-        ItemOre oilsands = ExNihiloRegistryManager.ORE_REGISTRY.getOreItem("oilsands");
-        ItemOre sphalerite = ExNihiloRegistryManager.ORE_REGISTRY.getOreItem("sphalerite");
-		ItemOre cobaltite = ExNihiloRegistryManager.ORE_REGISTRY.getOreItem("cobaltite");
-        
-        ItemStack crushedNetherrack = Item.getItemFromBlock(ModBlocks.netherrackCrushed).getDefaultInstance();
-        @SuppressWarnings("unused")
-		ItemStack crushedEndstone = Item.getItemFromBlock(ModBlocks.endstoneCrushed).getDefaultInstance();
-        
-        if (sulfur != null) {
-            registry.register(crushedNetherrack, new ItemInfo(sulfur), 0.05f, BlockSieve.MeshType.DIAMOND.getID());
-        }
-        
-        if (calcite != null) {
-            registry.register("dust", new ItemInfo(calcite), 0.02f, BlockSieve.MeshType.DIAMOND.getID());
-        }
-        
-        if (gypsum != null) {
-            registry.register("dust", new ItemInfo(gypsum), 0.02f, BlockSieve.MeshType.DIAMOND.getID());
-        }
-        
-        if (oilsands != null) {
-            registry.register("sand", new ItemInfo(oilsands), 0.01f, BlockSieve.MeshType.DIAMOND.getID());
-        }
-        
-        if (sphalerite != null) {
-            registry.register("gravel", new ItemInfo(sphalerite), 0.05f, BlockSieve.MeshType.DIAMOND.getID());
-        }
-        
-        if (cobaltite != null) {
-            registry.register("gravel", new ItemInfo(cobaltite), 0.05f, BlockSieve.MeshType.DIAMOND.getID());
-        }
+		addSieveRecipe(registry, Materials.Sulfur, ModBlocks.netherrackCrushed, 0.05f, BlockSieve.MeshType.DIAMOND);
+		addSieveRecipe(registry, Materials.Calcite, ModBlocks.dust, 0.02f, BlockSieve.MeshType.DIAMOND);
+		addSieveRecipe(registry, Materials.Gypsum, ModBlocks.dust, 0.02f, BlockSieve.MeshType.DIAMOND);
+		addSieveRecipe(registry, Materials.Oilsands, Blocks.SAND, 0.01f, BlockSieve.MeshType.DIAMOND);
+		addSieveRecipe(registry, Materials.Sphalerite, Blocks.GRAVEL, 0.05f, BlockSieve.MeshType.DIAMOND);
+		addSieveRecipe(registry, Materials.Cobaltite, Blocks.GRAVEL, 0.05f, BlockSieve.MeshType.DIAMOND);
+		addSieveRecipe(registry, Materials.Galena, Blocks.GRAVEL, 0.05f, BlockSieve.MeshType.DIAMOND);
+		addSieveRecipe(registry, Materials.Tungstate, ModBlocks.endstoneCrushed, 0.05f, BlockSieve.MeshType.DIAMOND);
+		addSieveRecipe(registry, Materials.Scheelite, ModBlocks.endstoneCrushed, 0.05f, BlockSieve.MeshType.DIAMOND);
     }
 	
 	@Override
@@ -127,14 +68,39 @@ public class ExNihiloGTModule implements IExNihiloCreatioModule, IRecipeDefaults
 		return GTValues.MODID;
 	}
 	
-	private void registerMaterial(OreRegistry registry, Material material)
+	private void addSieveRecipe(SieveRegistry registry, Material material, Block blockToSift, float chance, BlockSieve.MeshType meshType)
+	{
+		ItemOre materialOre = ExNihiloRegistryManager.ORE_REGISTRY.getOreItem(material.getName());
+		ItemStack block = Item.getItemFromBlock(blockToSift).getDefaultInstance();
+		
+		if(materialOre != null)
+		{
+			registry.register(block, new ItemInfo(materialOre), chance, meshType.getID());
+		}
+	}
+	
+	private void registerMaterial(OreRegistry registry, Material material, boolean dustOnly, boolean blacklist)
 	{
 		Item ingot = Item.getByNameOrId("gregtech:meta_ingot");
 		Item dust = Item.getByNameOrId("gregtech:meta_dust");
 		
-		registry.register(material.getName(), new Color(material.getMaterialRGB()), 
-				new ItemInfo(ingot, material.getId()),
-				new ItemInfo(dust, material.getId()));
+		if(dustOnly == true)
+		{
+			registry.register(material.getName(), new Color(material.getMaterialRGB()), 
+					new ItemInfo(dust, material.getId()),
+					new ItemInfo(dust, material.getId()));
+		}
+		else
+		{
+			registry.register(material.getName(), new Color(material.getMaterialRGB()), 
+					new ItemInfo(ingot, material.getId()),
+					new ItemInfo(dust, material.getId()));
+		}
+		
+		if(blacklist == true) // By default it adds everything to the gravel sieve so we blacklist it by default so it doesn't do that
+		{
+			ItemOre oreToAdd = ExNihiloRegistryManager.ORE_REGISTRY.getOreItem(material.getName());
+			registry.getSieveBlackList().add(oreToAdd);
+		}
 	}
-
 }
