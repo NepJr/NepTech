6143b8e16b2dd955480f1068e3c400135b847c28
NepJr <diehardsteam@gmail.com>
2026-05-15 11:54:57 -0700

    steam crusher

--- src/main/java/nepjr/tech/metatileentities/NTMetaTileEntities.java
+++ src/main/java/nepjr/tech/metatileentities/NTMetaTileEntities.java
@@ -4,6 +4,7 @@ import static gregtech.common.metatileentities.MetaTileEntities.registerMetaTile
 import static nepjr.tech.NepTech.nepId;
 
 import nepjr.tech.metatileentities.multi.electric.MetaTileEntityGreenhouse;
+import nepjr.tech.metatileentities.multi.steam.MetaTileEntitySteamCrusher;
 import nepjr.tech.metatileentities.multi.steam.MetaTileEntitySteamSmasher;
 
 public class NTMetaTileEntities 
@@ -13,6 +14,7 @@ public class NTMetaTileEntities
 	
 	// Steam multiblocks (7001 - 7100)
 	public static MetaTileEntitySteamSmasher[] STEAM_SMASHER = new MetaTileEntitySteamSmasher[2];
+	public static MetaTileEntitySteamCrusher[] STEAM_CRUSHER = new MetaTileEntitySteamCrusher[2];
 	
 	public static void register()
 	{
@@ -20,5 +22,8 @@ public class NTMetaTileEntities
 		
 		STEAM_SMASHER[0] = registerMetaTileEntity(7001, new MetaTileEntitySteamSmasher(1, nepId("steam_smasher")));
 		STEAM_SMASHER[1] = registerMetaTileEntity(7002, new MetaTileEntitySteamSmasher(2, nepId("hp_steam_smasher")));
+		
+		STEAM_CRUSHER[0] = registerMetaTileEntity(7003, new MetaTileEntitySteamCrusher(1, nepId("steam_crusher")));
+		STEAM_CRUSHER[1] = registerMetaTileEntity(7004, new MetaTileEntitySteamCrusher(2, nepId("hp_steam_crusher")));
 	}
 }
---
+++ src/main/java/nepjr/tech/metatileentities/multi/steam/MetaTileEntitySteamCrusher.java
@@ -0,0 +1,139 @@
+package nepjr.tech.metatileentities.multi.steam;
+
+import static gregtech.client.renderer.texture.Textures.BRONZE_PLATED_BRICKS;
+import static gregtech.client.renderer.texture.Textures.SOLID_STEEL_CASING;
+
+import java.util.List;
+
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import gregtech.api.gui.GuiTextures;
+import gregtech.api.gui.ModularUI;
+import gregtech.api.gui.widgets.AdvancedTextWidget;
+import gregtech.api.gui.widgets.IndicatorImageWidget;
+import gregtech.api.metatileentity.MetaTileEntity;
+import gregtech.api.metatileentity.interfaces.IGregTechTileEntity;
+import gregtech.api.metatileentity.multiblock.IMultiblockPart;
+import gregtech.api.metatileentity.multiblock.RecipeMapSteamMultiblockController;
+import gregtech.api.pattern.BlockPattern;
+import gregtech.api.pattern.FactoryBlockPattern;
+import gregtech.api.recipes.RecipeMaps;
+import gregtech.client.particle.VanillaParticleEffects;
+import gregtech.client.renderer.ICubeRenderer;
+import gregtech.client.renderer.texture.Textures;
+import gregtech.client.utils.TooltipHelper;
+import gregtech.common.blocks.BlockMetalCasing;
+import gregtech.common.blocks.MetaBlocks;
+import nepjr.tech.api.capability.impl.NTSteamMultiWorkable;
+import net.minecraft.block.state.IBlockState;
+import net.minecraft.client.resources.I18n;
+import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.EnumParticleTypes;
+import net.minecraft.util.ResourceLocation;
+import net.minecraft.world.World;
+import net.minecraftforge.fml.relauncher.Side;
+import net.minecraftforge.fml.relauncher.SideOnly;
+
+public class MetaTileEntitySteamCrusher extends RecipeMapSteamMultiblockController {
+
+	protected final int level;
+	protected final int PARALLEL_LIMIT;
+
+    public MetaTileEntitySteamCrusher(int level, ResourceLocation metaTileEntityId) {
+		super(metaTileEntityId, RecipeMaps.MACERATOR_RECIPES, CONVERSION_RATE);
+		this.level = level;
+        this.PARALLEL_LIMIT = 8 * level;
+        this.recipeMapWorkable = new NTSteamMultiWorkable(this, CONVERSION_RATE, level);
+        this.recipeMapWorkable.setParallelLimit(PARALLEL_LIMIT);
+    }
+
+    @Override
+    public MetaTileEntity createMetaTileEntity(IGregTechTileEntity metaTileEntityHolder) {
+        return new MetaTileEntitySteamCrusher(level, metaTileEntityId);
+    }
+
+    @Override
+    protected BlockPattern createStructurePattern() {
+        return FactoryBlockPattern.start()
+                .aisle("XXX", "XXX", "XXX")
+                .aisle("XXX", "XAX", "XXX")
+                .aisle("XXX", "XSX", "XXX")
+                .where('S', selfPredicate())
+                .where('X', states(getCasingState()).setMinGlobalLimited(14).or(autoAbilities()))
+                .where('A', air())
+                .build();
+    }
+
+    public IBlockState getCasingState() {
+	      if(level == 2)
+	    	  return MetaBlocks.METAL_CASING.getState(BlockMetalCasing.MetalCasingType.STEEL_SOLID);
+	      else
+	    	  return MetaBlocks.METAL_CASING.getState(BlockMetalCasing.MetalCasingType.BRONZE_BRICKS);
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public ICubeRenderer getBaseTexture(IMultiblockPart sourcePart) {
+        if (level == 2)
+        {
+        	return SOLID_STEEL_CASING;
+        }
+        else
+        {
+        	return BRONZE_PLATED_BRICKS;
+        }
+    }
+
+    @SideOnly(Side.CLIENT)
+    @NotNull
+    @Override
+    protected ICubeRenderer getFrontOverlay() {
+        return Textures.ROCK_BREAKER_OVERLAY;
+    }
+    
+    @Override
+    public boolean hasMaintenanceMechanics() {
+        return false;
+    }
+
+    @Override
+    public int getItemOutputLimit() {
+        return 1;
+    }
+
+    @Override
+    public void addInformation(ItemStack stack, @Nullable World player, @NotNull List<String> tooltip,
+                               boolean advanced) {
+        super.addInformation(stack, player, tooltip, advanced);
+        tooltip.add(I18n.format("gregtech.universal.tooltip.parallel", PARALLEL_LIMIT));
+        tooltip.add(TooltipHelper.BLINKING_ORANGE + I18n.format("gregtech.multiblock.require_steam_parts"));
+    }
+    
+    @Override
+    protected ModularUI.Builder createUITemplate(EntityPlayer entityPlayer) {
+        ModularUI.Builder builder = ModularUI
+                .builder(GuiTextures.BACKGROUND_STEAM.get((level == 2) ? true : false), 176, 208);
+        builder.shouldColor(false);
+        builder.image(4, 4, 168, 117, GuiTextures.DISPLAY_STEAM.get((level == 2) ? true : false));
+        builder.label(9, 9, getMetaFullName(), 0xFFFFFF);
+        builder.widget(new AdvancedTextWidget(9, 20, this::addDisplayText, 0xFFFFFF)
+                .setMaxWidthLimit(162)
+                .setClickHandler(this::handleDisplayClick));
+        builder.widget(new IndicatorImageWidget(152, 101, 17, 17, getLogo())
+                .setWarningStatus(getWarningLogo(), this::addWarningText)
+                .setErrorStatus(getErrorLogo(), this::addErrorText));
+        builder.bindPlayerInventory(entityPlayer.inventory,
+                GuiTextures.SLOT_STEAM.get((level == 2) ? true : false), 7, 125);
+        return builder;
+    }
+
+    @SideOnly(Side.CLIENT)
+    @Override
+    public void randomDisplayTick() {
+        if (isActive()) {
+            VanillaParticleEffects.defaultFrontEffect(this, 0.4F, EnumParticleTypes.SMOKE_NORMAL);
+        }
+    }
+}
