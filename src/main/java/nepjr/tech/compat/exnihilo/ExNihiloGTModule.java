71f01a433526c8f904c95b5dfcced6323e48f775
NepJr <diehardsteam@gmail.com>
2026-06-04 01:37:06 -0700

    redstone 2

--- src/main/java/nepjr/tech/compat/exnihilo/ExNihiloGTModule.java
+++ src/main/java/nepjr/tech/compat/exnihilo/ExNihiloGTModule.java
@@ -46,6 +46,7 @@ public class ExNihiloGTModule implements IExNihiloCreatioModule, IRecipeDefaults
 		registerMaterial(registry, Materials.Oilsands, true, true);
 		registerMaterial(registry, Materials.Cassiterite, true, true);
 		registerMaterial(registry, Materials.Garnierite, true, true);
+		registerMaterial(registry, Materials.Redstone, true, true);
 		
 		// Gems
 		registerMaterial(registry, Materials.Ruby, true, true);
